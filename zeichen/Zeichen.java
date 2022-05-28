f54ce145767fb71c5afee8cc9feb18f401b96b7c
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-05-28 13:08:53 +0000

    Zeichen

---
+++ zeichen/Zeichen.java
@@ -0,0 +1,16 @@
+package zeichen;
+
+public class Zeichen {
+	public static void main(String[] args) {
+		char c;
+		int d = 0;
+		c = '3';
+		System.out.println(c);
+
+		c = 49;
+		System.out.println(c);
+		System.out.println(d);
+		d = d + c;
+		System.out.println(d);
+	}
+}
