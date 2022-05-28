ac9010e50a6734956025dc1c64baeb808ec104de
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-05-28 12:50:54 +0000

    phrasOmat

---
+++ phrasOMat/phrasOMat.java
@@ -0,0 +1,41 @@
+package phrasOMat;
+
+class PhrasOMat {
+	public static void main(String args[]) {
+		 String[] wortListEins = {
+				"verlässliche",
+				"erfolhsorinetierte",
+				"verteilte",
+				"zielgerichtete"
+		};
+
+		String[] wortListeZwei = {
+				"Out-of-the-Box-",
+				"kraftvolle ",
+				"Multi-Tier-",
+				"kooperative ",
+				"B2B-"
+		};
+
+		String[] wortListeDrei = {
+				"Schicht",
+				"Vision",
+				"Mission",
+				"Strategie",
+				"Lösung"
+		};
+
+		int einsLength = wortListEins.length;
+		int zweiLength = wortListeZwei.length;
+		int dreiLength = wortListeDrei.length;
+
+		int rand1 = (int) (Math.random() * einsLength);
+		int rand2 = (int) (Math.random() * zweiLength);
+		int rand3 = (int) (Math.random() * dreiLength);
+
+
+		String phrase = wortListEins[rand1] + wortListeZwei[rand2] + wortListeDrei[rand3];
+
+		System.out.println(phrase);
+	}
+}
